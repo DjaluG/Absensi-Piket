910157d039957d9750e738a2ea88b200a60c7221
Abisena <abisena1123@gmail.com>
2023-08-13 19:09:18 +0700

    tools_ctm

--- piket_api/api.py
+++ piket_api/api.py
@@ -3,6 +3,8 @@ from models.Student import Student
 from fastapi.encoders import jsonable_encoder
 from bson import ObjectId
 from database.db import db_connection
+# import csv
+# from pymongo import MongoClient
 
 router = APIRouter()
 
@@ -14,7 +16,6 @@ async def read_api():
     }
 
 
-# Get All Students
 @router.get('/students')
 async def get_all_students():
     collection = db_connection()['students']
@@ -24,51 +25,60 @@ async def get_all_students():
         # Convert ObjectId to string
         student['_id'] = str(student['_id'])
         student_list.append(student)
-    
+
     return student_list
 
 # Delete
+
+
 @router.delete('/students/{student_id}')
 async def delete_students(student_id: str):
-      collection = db_connection()['students']
-      # Temukan Id Student dan delete  
-      result = collection.delete_one({"_id" : ObjectId(student_id)})
+    collection = db_connection()['students']
+    # Temukan Id Student dan delete
+    result = collection.delete_one({"_id": ObjectId(student_id)})
 
-      if result.deleted_count == 1:
-           return {"message": f"Student Succesffully deleted"}
-      else:
-           return {"message": f"Student  Not Found"}
+    if result.deleted_count == 1:
+        return {"message": f"Student Succesffully deleted"}
+    else:
+        return {"message": f"Student  Not Found"}
 
 # Get Id Student
+
+
 @router.get('/students/{student_id}')
 async def get_specific_student(student_id: str):
     collection = db_connection()['students']
-    
+
     # Temukan data berdasarkan ObjectId
     result = collection.find_one({"_id": ObjectId(student_id)})
-    
+
     if result:
         # Convert ObjectId to String
         result['_id'] = str(result['_id'])
         return jsonable_encoder(result)
     else:
         return {"message": "Student not found"}
-    
+
 # Update
+
+
 @router.put('/students/{student_id}')
 async def update_student(student_id: str, updated_student: Student):
     collection = db_connection()['students']
-    result = collection.update_one({"_id": ObjectId(student_id)}, {"$set": updated_student.dict()})
-    
+    result = collection.update_one({"_id": ObjectId(student_id)}, {
+                                   "$set": updated_student.dict()})
+
     if result.modified_count == 1:
         return {"message": f"Student Updated"}
     else:
         return {"message": f"Student not found"}
 
 # Create
+
+
 @router.post('/students')
 async def create_student(student: Student):
     collection = db_connection()['students']
     result = collection.insert_one(student.dict())
-    
+
     return {"message": "Student created", "student_id": str(result.inserted_id)}
